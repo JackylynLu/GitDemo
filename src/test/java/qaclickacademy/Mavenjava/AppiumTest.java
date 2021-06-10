62cf5ac85cd8d6d0add72c3763937a6b3b25c479
Jackie <jackylyn@gmail.com>
2021-06-10 14:22:06 +0800

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NavieAppAndroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+	
+}
